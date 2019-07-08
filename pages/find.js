2479bfb5b7a8edeacab5599a827b0fc86f2750c7
周群华 <qunhua.zhou@audaque.com>
2019-07-08 17:50:58 +0800

    find

--- pages/find.js
+++ pages/find.js
@@ -15,5 +15,34 @@ Page({
   },
   onUnload() {
 
+  },
+  chooseLocation() {
+    wx.chooseLocation({
+      success: (res) => {
+        wx.showToast({
+          title: JSON.stringify(res),
+          icon: 'none',
+          duration: 2000
+        })
+      },
+      fail: (error) => {
+        wx.showToast({
+          title: JSON.stringify(error),
+          icon: 'none',
+          duration: 2000
+        });
+      },
+    })
+  },
+  scanCode() {
+    wx.scanCode({
+      success: (res) => {
+        my.showToast({
+          title: JSON.stringify(res),
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    })
   }
 })
